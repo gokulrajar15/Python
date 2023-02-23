f02164ee8dc0b143eebf3e4416f9bb38827fd0bd
gokulrajar15 <gokulrajar15@gmail.com>
2023-02-23 19:05:46 +0530

    second commit

--- 23-02-23 File handing Assesment.py
+++ 23-02-23 File handing Assesment.py
@@ -30,3 +30,21 @@ with open("C:\\Users\\Gokul\\Documents\\practice.txt.txt", "r") as file2:
     content2 = file2.read()
     Vowels = content2.count("a") + content2.count("A") + content2.count("e") + content2.count("E") + content2.count("i") + content2.count("I") + content2.count("o") + content2.count("O") + content2.count("u") + content2.count("U")
     print("The count of vowels are {}".format(Vowels))
+print("--------------------------------------------------------------------------------------------------")
+
+#5(a). Write a statement in Python to open text file NEWS. TXT so that new content can be written
+
+with open("C:\\Users\\Gokul\\Documents\\NEWS.txt", "w") as file3:
+    content3 = file3.write("Hello this is News channel")
+    file3.close()
+
+#5(b). Write a statement in Python to open text file NEWS. TXT so that existing content can be read
+with open("C:\\Users\\Gokul\\Documents\\NEWS.txt", "r") as file4:
+    print(file4.read())
+    file4.close()
+
+#5(c). Write a statement in Python to open text file NEWS. TXT so that new content can be written after
+with open("C:\\Users\\Gokul\\Documents\\NEWS.txt", "a") as file5:
+    content5 = file5.write(" \n Today the weather going to severe rain")
+    file5.close()
+

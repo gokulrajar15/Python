892200b8f978b93800782052b92aeb35b9f8737b
gokulrajar15 <gokulrajar15@gmail.com>
2023-03-06 12:33:46 +0530

    oops

---
+++ Oops.py
@@ -0,0 +1,20 @@
+class Oops:
+    a = 40
+    b = 12
+    add = a + b
+    sub = a - b
+    mul = a * b
+    div = a / b
+
+
+addition = Oops()
+print("The addition of two variables is {}".format(addition.add))
+
+subtract = Oops()
+print("The subtraction of two variables is {}".format(subtract.sub))
+
+multiplication = Oops()
+print("The multiplication of two variables is {}".format(multiplication.mul))
+
+division = Oops()
+print("The division of two variables is {}".format(division.div))

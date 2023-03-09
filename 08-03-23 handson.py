2e9c15f7217764a75c91a2a760f92ae504c19c2a
gokulrajar15 <gokulrajar15@gmail.com>
2023-03-09 11:14:02 +0530

    assesment

--- 08-03-23 handson.py
+++ 08-03-23 handson.py
@@ -1,11 +1,9 @@
 class Marks:
+    #constructor
     def __init__(self):
-        self.Tamil = 0
-        self.English = 0
-        self.Maths = 0
-        self.Science = 0
-        self.Social = 0
+        self.Tamil, self.English, self.Maths, self.Science, self.Social = 0, 0, 0, 0, 0
 
+    # for receiving marks
     def enter_marks(self, Tamil, English, Maths, Science, Social):
         self.Tamil = Tamil
         self.English = English
@@ -13,6 +11,7 @@ class Marks:
         self.Science = Science
         self.Social = Social
 
+    # for displaying marks
     def display_marks(self):
         print("Tamil = {}".format(self.Tamil))
         print("English = {}".format(self.English))
@@ -21,16 +20,16 @@ class Marks:
         print("Social = {}".format(self.Social))
 
 
-class average(Marks):
-    def display_total_average(self):
-        Total = (self.Tamil + self.English + self.Maths + self.Science + self.Social)
-        average = Total/5
-        print("The total mark is {}".format(Total))
+class average:
+    def display_total_and_Avg(self):
+        Total = self.Tamil + self.English + self.Maths + self.Science + self.Social
+        print("The Total mark of {} is {}".format(self.name, Total))
+        average = Total / 5
         print("The Average mark is {}".format(average))
 
 
 
-class Student(average):
+class Student(average, Marks):
     def __init__(self, name, s_class, sex, ph_number, address):
         self.name = name
         self.s_class = s_class
@@ -47,9 +46,9 @@ class Student(average):
 
 
 print("------------Students Details------------------------")
-gokul = Student("Gokul", "10th", "Male", "999230202", "mettur,salem")
+gokul = Student("Gokul", "10th", "Male", "999230202", "mettur, salem")
 gokul.display_details()
 print("------------------Marks------------------------")
 gokul.enter_marks(37, 45, 89, 23, 45)
 gokul.display_marks()
-gokul.display_total_average()
+gokul.display_total_and_Avg()
